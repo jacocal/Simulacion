ab53691177d7723842d9fd9f2e9d837fbecb827b
Jacobo Calderon <cacovoltqgmail.com>
2018-03-22 14:18:09 -0600

    Random arreglado

--- Actividades/12-RandomGenerator/generator.py
+++ Actividades/12-RandomGenerator/generator.py
@@ -1,6 +1,7 @@
 class EBI:
     results = []
-    digits = 4
+    valid = []
+    digits = 16
     def __init__(self):
         with open("seed.txt", 'r+') as file:
             c = file.readlines()
@@ -11,18 +12,28 @@ class EBI:
             new = int(self.seed[x]) ** 2
             if(new <= 1):
                 new += 1
-            self.seed[x] = str(new)[2:-2]
+            self.seed[x] = str(new)[8:-8]
             while(len(self.seed[x]) < self.digits):
                 self.seed[x] = '1' + self.seed[x]
 
     def appResult(self):
-        new = int(''.join(self.seed))
-        self.nextRand()
-        while(new not in self.results):
+        new = '0' * 16
+        while(new not in self.results and len(self.valid) < 1000):
+            cont = 0
             self.results.append(new)
+            for d in range(self.digits-1, -1, -1):
+                if(d % 2 == 0):
+                    cont += int(new[d]) * 2
+                else:
+                    cont += int(new[d])
+            if(cont % 10):
+                self.valid.append(new)
             self.nextRand()
-            new = int(''.join(self.seed))
-        return(len(self.results))
+            new = ''.join(self.seed)
+        with open("validas.txt", 'w') as file:
+            for v in self.valid:
+                file.write(v+"\n")
+        return(self.valid[-10:])
 
 def main():
     e = EBI()
